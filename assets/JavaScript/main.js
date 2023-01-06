9d5fd14e5a0fbd36a0dfaa8665b36e87550ab8b8
CatalinBarbu <121017880+CatalinBarbu@users.noreply.github.com>
2023-01-06 15:29:51 +0100

    First commit

---
+++ assets/JavaScript/main.js
@@ -0,0 +1,16 @@
+function intro3(name, stadt, alter) {
+  name = "Catalin";
+  stadt = "Bad Mergentheim";
+  alter = "24";
+  console.log(
+    "Hallo, mein Name ist " +
+      name +
+      ". Ich bin " +
+      alter +
+      " alt. " +
+      "Ich komme aus " +
+      stadt +
+      "."
+  );
+}
+intro3();
